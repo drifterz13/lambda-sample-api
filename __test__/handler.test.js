a1ac6436444ef4064b982b524f81e962daff3a91
Mix Mr <totsawat@vertice-international.com>
2018-08-10 11:30:05 +0700

    Add more update/delete handler, fix cors

--- __test__/handler.test.js
+++ __test__/handler.test.js
@@ -6,7 +6,7 @@ const handler = require('../handler')
 const Tweet = require('../models/Tweet')
 
 describe('Get all tweets', () => {
-  it('should return all tweets', () => {
+  it('should return all tweets', (done) => {
     const TweetMock = sinon.mock(Tweet)
     const expectResult = {status: true, tweets: []}
     TweetMock.expects('find').yields(null, expectResult)
@@ -14,10 +14,11 @@ describe('Get all tweets', () => {
       TweetMock.verify()
       TweetMock.restore()
       expect(err).toBeFalsy
+      done()
     })
   })
 
-  it('should return error, if cant get tweets', () => {
+  it('should return error, if cant get tweets', (done) => {
     const TweetMock = sinon.mock(Tweet)
     const expectResult = {status: false, error: 'Not found!'}
     TweetMock.expects('find').yields(expectResult, null)
@@ -25,6 +26,7 @@ describe('Get all tweets', () => {
       TweetMock.verify()
       TweetMock.restore()
       expect(err).toBeTruthy
+      done()
     })
   })
 })
@@ -35,23 +37,80 @@ describe('Create new tweet', () => {
     TweetMock = sinon.mock(new Tweet({ text: 'Hello World!' }))
     tweet = TweetMock.object
   })
-  it('should create new tweet', () => {
+  it('should create new tweet', (done) => {
     const expectResult = {status: true}
     TweetMock.expects('save').yields(null, expectResult)
     tweet.save((err, result) => {
       TweetMock.verify()
       TweetMock.restore()
       expect(result.status).toBeTruthy
+      done()
     })
   })
 
-  it('should return error, if tweet not save', () => {
+  it('should return error, if tweet not save', (done) => {
     const expectResult = {status: false}
     TweetMock.expects('save').yields(expectResult, null)
     tweet.save((err, result) => {
       TweetMock.verify()
       TweetMock.restore()
       expect(err.status).toBeFalsy
+      done()
+    })
+  })
+})
+
+describe('Update tweet', () => {
+  let TweetMock, tweet
+  beforeEach(() => {
+    TweetMock = sinon.mock(new Tweet({ text: 'Nothing' }))
+    tweet = TweetMock.object
+  })
+  it('should update tweet', (done) => {
+    const expectResult = {status: true}
+    TweetMock.expects('save').withArgs({_id: 123}).yields(null, expectResult)
+    tweet.save({_id: 123}, (err, result) => {
+      TweetMock.verify()
+      TweetMock.restore()
+      expect(result.status).toBeTruthy
+      done()
+    })
+  })
+
+  it('should return error, if tweet not update', (done) => {
+    const expectResult = { status: false }
+    TweetMock.expects('save').withArgs({ _id: 123 }).yields(expectResult, null)
+    tweet.save({_id: 123}, (err, result) => {
+      TweetMock.verify()
+      TweetMock.restore()
+      expect(err.status).toBeFalsy
+      done()
+    })
+  })
+})
+
+describe('Remove tweet', () => {
+  it('should remove tweet', (done) => {
+    const TweetMock = sinon.mock(Tweet)
+    const expectResult = {status: true}
+    TweetMock.expects('remove').withArgs({_id: 123}).yields(null, expectResult)
+    Tweet.remove({_id: 123}, (err, result) => {
+      TweetMock.verify()
+      TweetMock.restore()
+      expect(result.status).toBeTruthy
+      done()
+    })
+  })
+
+  it('shoul return error, if tweet not remove', (done) => {
+    const TweetMock = sinon.mock(Tweet)
+    const expectResult = {status: false}
+    TweetMock.expects('remove').withArgs({_id: 123}).yields(expectResult, null)
+    Tweet.remove({_id: 123}, (err, result) => {
+      TweetMock.verify()
+      TweetMock.restore()
+      expect(err.status).toBeFalsy
+      done()
     })
   })
 })
--- handler.js
+++ handler.js
@@ -7,15 +7,23 @@ module.exports.createTweet = (event, context, callback) => {
 
   connectToDatabase().then(() => {
     Tweet.create(JSON.parse(event.body))
-      .then(tweet => callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(tweet)
-      }))
-      .catch(err => callback(null, {
-        statusCode: err.statusCode || 500,
-        headers: { "Content-Type": "text/plain" },
-        body: "Not found!"
-      }));
+      .then(tweet =>
+        callback(null, {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+          },
+          body: JSON.stringify(tweet)
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Not found!"
+        })
+      );
   });
 };
 
@@ -24,14 +32,76 @@ module.exports.getTweets = (event, context, callback) => {
 
   connectToDatabase().then(() => {
     Tweet.find()
-      .then(tweets => callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(tweets)
-      }))
-      .catch(err => callback(null, {
-        statusCode: err.statusCode || 500,
-        headers: { "Content-Type": "text/plain" },
-        body: "Not found!"
-      }))
+      .then(tweets =>
+        callback(null, {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+          },
+          body: JSON.stringify(tweets)
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Not found!"
+        })
+      );
+  });
+};
+
+module.exports.updateTweet = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Tweet.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), {
+      new: true
+    })
+      .then(tweet =>
+        callback(null, {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+          },
+          body: JSON.stringify(tweet)
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Not found!"
+        })
+      );
+  });
+};
+
+module.exports.deleteTweet = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Tweet.findByIdAndRemove(event.pathParameters.id)
+      .then(tweet =>
+        callback(null, {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+          },
+          body: JSON.stringify({
+            message: `tweet id: ${event.pathParameters.id} has been deleted`
+          })
+        })
+      )
+      .catch(err =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Not found!"
+        })
+      );
   });
 };
